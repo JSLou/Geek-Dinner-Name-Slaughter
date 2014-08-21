3a684fcb623bdf98b4e01408cba580e90e072603
Alex Bezuska <abezuska@gmail.com>
2014-08-20 20:14:44 -0400

    add hit increminter, add fix for favicon request

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var app = express();
+var hits = 0;
+
+app.use("/favicon.ico", function(req, res, next) {
+	res.end();
+});
 
 app.use(function(req, res, next) {
-	res.send("Hello JSLou!");
+	hits++;
+	res.send("Hello JSLou! hits: " + hits);
 	res.end();
 });
 
