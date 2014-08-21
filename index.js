c8654694b06cf7d878745afa92fe704562d0962a
Alex Bezuska <abezuska@gmail.com>
2014-08-20 20:32:42 -0400

    add names object

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 var express = require("express");
 var app = express();
 var hits = 0;
+var names = {
+	"Alex's Deli": 0,
+	"Herp Derp": 0,
+	"function.length": 0
+};
 
-app.use("/favicon.ico", function(req, res, next) {
-	res.end();
+app.use("/favicon.ico", function(request, response, next) {
+	response.end();
 });
 
-app.use(function(req, res, next) {
+app.use("/reset", function(request, response, next) {
+	hits = 0;
+	response.redirect("/");
+});
+
+app.use(function(request, response, next) {
 	hits++;
-	res.send("Hello JSLou! hits: " + hits);
-	res.end();
+	response.send("Hello JSLou! hits: " + hits);
+	response.end();
 });
 
 app.listen(3000, function() {
